9275801f66ef9077eb4113f4bd0d60b5de13aa32
João Sanson <joaosanson01@gmail.com>
2024-01-13 21:43:23 -0300

    creating tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import http from 'node:http'
+import handler from './handler.js'
 
 const PORT = process.env.PORT || 3333
 
 const server = http
-  .createServer((request, response) => {
-    response.end('Hello World')
-  })
+  .createServer(handler)
   .listen(PORT, () => console.log(`Server is running at ${PORT}`))
 
 export { server }
